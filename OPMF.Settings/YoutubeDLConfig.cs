4821b114613b83aa3b5f194317f8b0a2ad4a48ad
Junying Chen <jygchen@gmail.com>
2021-05-09 19:02:27 +1000

    Aviod downloading youtube subtitles by default.

--- OPMF.Settings/YoutubeDLConfig.cs
+++ OPMF.Settings/YoutubeDLConfig.cs
@@ -3,7 +3,7 @@
     public class YoutubeDLConfig
     {
         public bool CheckForBinaryUpdates { get; set; } = true;
-        public bool GetSubtitles { get; set; } = true;
+        public bool GetSubtitles { get; set; } = false;
         public string VideoExtension { get; set; } = "mp4";
         public string SubtitleExtension { get; set; } = "vtt";
     }
